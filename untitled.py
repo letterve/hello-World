d4c2cca4261b47f16e4447fb76773a660291298d
m <sedvic@protonmail.com>
2017-05-27 15:22:59 +0200

    en till

---
+++ untitled.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+count = 0
+
+while (count<5):
+	print "Hello World"
+	count = count + 1
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
